173252e921c4efad0f3aa586a9bdce6099e50f43
Rafakosz <rafakosz@poczta.onet.pl>
2020-05-03 18:35:23 +0200

    comments added

--- examples/simple_led_via_serial/simple_led_via_serial.ino
+++ examples/simple_led_via_serial/simple_led_via_serial.ino
@@ -19,28 +19,34 @@ void serialEvent() {
     }
   }
 }
-
+// initializing PWMSmoothChange object as pwm with a max PWM value of 255
 PWMSmoothChange pwm(255);
+
 
 void setup(){
   Serial.begin(9600);
   inputString.reserve(100);
   while (!Serial);
   pinMode(ledPIN, OUTPUT);
+  // setup transition time for about 10 s (from 0 to 100%)
   pwm.setup(10240);
 }
 
 void loop(){
+  // wait for string with a new value from 0 to 100
   if (stringComplete) {
     Serial.print("Final PWM: ");
     int newValue = inputString.toInt();
     if (newValue >= 0) {
+      // passing the new value to pwm object
       pwm.setLightValue(newValue);
       Serial.println(pwm.iFinalPWM);
     }
     stringComplete = false;
     inputString = "";
   }
+  // in every loop checks if new PWM value should be change and if yes 
+  //  writes to the specified pin with analogWrite()
   if (pwm.check()) {
     analogWrite(ledPIN, pwm.iNewPWM);
   }
